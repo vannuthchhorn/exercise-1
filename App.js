8d3e3552735790d31b1abf85511df386eec7bec8
vannuth chhorn <vannuthchhorn4@gmail.com>
2022-09-08 13:40:41 +0700

    commit project

---
+++ App.js
@@ -0,0 +1,12 @@
+// import Login from "login/Login"
+import Register from "./register/Register";
+import React from "react";
+
+export default () => {
+  return(
+    <React.Fragment>
+      <Register></Register>
+      
+    </React.Fragment>
+  )
+}
